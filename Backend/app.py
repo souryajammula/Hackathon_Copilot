5b1577baa14b4c18838df5f5da0a3a98a7e9bd8f
M.V.N.S.Prudhvi <34057003+prudhvimvns@users.noreply.github.com>
2023-11-11 18:24:22 -0800

    chatbot commit

---
+++ Backend/app.py
@@ -0,0 +1,96 @@
+
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from typing import Optional
+import os
+import logging
+
+app = FastAPI()
+
+# Create a logger instance
+logger = logging.getLogger(__name__)
+
+
+OPENAI_API_KEY = "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"
+
+TAX_PDF_PATH = "/Applications/XAMPP/xamppfiles/htdocs/Hackathon-copilot/Backend/taxpdf.pdf"
+# TAX_PDF_PATH = "/Applications/XAMPP/xamppfiles/htdocs/Hackathon-copilot/Backend/tax1.pdf"
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"], 
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+class ChatRequest(BaseModel):
+    user_input: str
+
+class Message(BaseModel):
+    user: str
+    text: str
+
+chat_history = []  # In-memory storage for chat messages
+
+def chat_with_openai(user_input):
+    try:
+        os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
+
+        # Read the PDF file in binary mode
+        with open(TAX_PDF_PATH, 'rb') as pdf_file:
+            pdf_text = pdf_file.read().decode('utf-8', errors='ignore')
+
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=800,
+            chunk_overlap=200,
+            length_function=len,
+        )
+        texts = text_splitter.split_text(pdf_text)
+        embeddings = OpenAIEmbeddings()
+        document_search = FAISS.from_texts(texts, embeddings)
+        chain = load_qa_chain(OpenAI(), chain_type="stuff")
+        docs = document_search.similarity_search(user_input)
+        result = chain.run(input_documents=docs, question=user_input)
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error processing user input: {str(e)}")
+
+
+@app.post("/chat")
+async def chat_with_user_input(chat_request: ChatRequest):
+    try:
+        response = chat_with_openai(chat_request.user_input)
+
+        # Save the chat message to the in-memory storage
+        chat_history.append(Message(user="User", text=chat_request.user_input))
+        chat_history.append(Message(user="Bot", text=response))
+
+        return {"response": response}
+    except Exception as e:
+        logger.exception(e)
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+
+@app.get("/get_chat_history")
+async def get_chat_history():
+    return chat_history
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG,
+                    handlers=[logging.FileHandler("debug.log"),
+                              logging.StreamHandler()])
+
+
+
+
+
+
+# # # uvicorn app:app --reload --host 0.0.0.0 --port 8000
--- dashboard/src/components/chatGPT.jsx
+++ dashboard/src/components/chatGPT.jsx
@@ -1,13 +1,149 @@
-import React from "react";
+// import React from "react";
+// import "./chatGPT.css"; 
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
+
+// const ChatGPT = () =>{
+
+
+//     const openChatbotModal = () => {
+//         // Trigger Bootstrap modal by its ID
+//         const modal = document.getElementById("chatbotModal");
+//         if (modal) {
+//           modal.style.display = "block";
+//         }
+//     };
+
+//     const closeChatbotModal = () => {
+//         // Close Bootstrap modal by its ID
+//         const modal = document.getElementById("chatbotModal");
+//         if (modal) {
+//           modal.style.display = "none";
+//         }
+//       };
+    
+
+//     return (
+//         <div>
+//             <h1>Chat GPT</h1>
+
+
+//             <button onClick={openChatbotModal} className="btn btn-primary">
+//         Open Chatbot
+//       </button>
+
+
+// {/* Bootstrap Modal */}
+// <div className="modal" id="chatbotModal">
+//         <div className="modal-dialog modal-lg">
+//           <div className="modal-content">
+//             {/* Modal Header */}
+//             <div className="modal-header">
+//               <h4 className="modal-title">Chatbot</h4>
+//               <button
+//                 type="button" class="close btn btn-light" className="close" data-dismiss="modal" aria-label="Close" onClick={closeChatbotModal}>
+//                 &times;
+//               </button>
+//             </div>
+
+//             {/* Modal Body */}
+//             <div className="modal-body">
+//               {/* Your chatbot UI content goes here */}
+//               <div className="chatbot-container">
+//                 {/* Chatbot messages and input */}
+//                 {/* ... */}
+
+//                 <div>
+//                     <div className="chat-history">
+//                         <div className="message">Hello, how can I help you?</div>
+//                         <div className="message">This is a response.</div>
+//                         <div className="message">Hello, how can I help you?</div>
+//                         <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
+//                         <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
+//                         <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
+//                         <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
+//                         <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
+//                         <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
+//                         <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
+//                         <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
+//                         <div className="message">This is a response.</div>
+//                     </div>
+//                 </div>
+
+//                 <div className="row userInputClass">
+//                     <div className="col-lg-10">
+//                         <input className="margin20" type="text" id="inputTextBox" name="inputTextBox"  placeholder="Type a new Question(eg:- Does my plan cover annual eye exams?)🙋‍♂️" />
+//                     </div>
+
+//                     <div className="submitClass col-lg-2">
+//                         <button type="button" class="btn btn-primary">Submit</button>
+//                     </div>
+//                 </div>
+
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+
+
+//         </div>
+//     )
+// }
+
+// export default ChatGPT;
+
+
+
+
+
+
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
 import "./chatGPT.css"; 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 const ChatGPT = () =>{
 
+    const [userInput, setUserInput] = useState('');
+    const [chatHistory, setChatHistory] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const chatContainerRef = useRef(null);
+    const scrollToBottom = useRef(null);
+
+    useEffect(()=>{
+        console.log("FFFFFFFFF");
+        setUserInput('');
+    },[]);
+
+    useEffect(() => {
+        if (scrollToBottom.current) {
+          scrollToBottom.current.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, [chatHistory]);
+    
+    
+      useEffect(() => {
+        if (chatContainerRef.current) {
+          chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
+      }, [chatHistory]);
+
+
+      const LoadingAnimation = () => {
+        return (
+          <div className="loading-animation">
+            <img src="/loading.gif" alt="Loading..." />
+            <div className="loading-text">Loading...</div>
+          </div>
+        );
+      };
+
 
     const openChatbotModal = () => {
-        // Trigger Bootstrap modal by its ID
         const modal = document.getElementById("chatbotModal");
         if (modal) {
           modal.style.display = "block";
@@ -15,10 +151,38 @@ const ChatGPT = () =>{
     };
 
     const closeChatbotModal = () => {
-        // Close Bootstrap modal by its ID
         const modal = document.getElementById("chatbotModal");
         if (modal) {
           modal.style.display = "none";
+        }
+      };
+
+
+    const backendCall = async () =>{
+        setIsLoading(true);
+        try{
+            console.log(userInput);
+            const res = await axios.post("http://localhost:8000/chat",{
+                user_input: userInput,
+            })
+        }
+        catch (error) {
+            console.log(error);
+        }
+        finally{
+            setIsLoading(false);
+            setUserInput('');
+            getChatHistory();
+        }  
+    };
+
+    const getChatHistory = async () => {
+        try {
+          const res = await axios.get('http://localhost:8000/get_chat_history');
+          setChatHistory(res.data);
+          console.log("RES:::::",res);
+        } catch (error) {
+          console.error('Error fetching chat history:', error);
         }
       };
     
@@ -27,17 +191,14 @@ const ChatGPT = () =>{
         <div>
             <h1>Chat GPT</h1>
 
-
-            <button onClick={openChatbotModal} className="btn btn-primary">
-        Open Chatbot
-      </button>
-
+            <button onClick={openChatbotModal} className="btn btn-primary">Open Chatbot</button>
+            <button onClick={backendCall} className="btn btn-primary">Call</button>
 
 {/* Bootstrap Modal */}
 <div className="modal" id="chatbotModal">
         <div className="modal-dialog modal-lg">
           <div className="modal-content">
-            {/* Modal Header */}
+
             <div className="modal-header">
               <h4 className="modal-title">Chatbot</h4>
               <button
@@ -46,37 +207,37 @@ const ChatGPT = () =>{
               </button>
             </div>
 
-            {/* Modal Body */}
             <div className="modal-body">
-              {/* Your chatbot UI content goes here */}
+
               <div className="chatbot-container">
-                {/* Chatbot messages and input */}
-                {/* ... */}
+
 
                 <div>
                     <div className="chat-history">
-                        <div className="message">Hello, how can I help you?</div>
-                        <div className="message">This is a response.</div>
-                        <div className="message">Hello, how can I help you?</div>
-                        <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
-                        <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
-                        <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
-                        <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
-                        <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
-                        <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
-                        <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
-                        <div className="message">This is a response.</div><div className="message">Hello, how can I help you?</div>
-                        <div className="message">This is a response.</div>
+                        <div className="responseClass chat-container" ref={chatContainerRef}>
+                        {chatHistory.map((message, index) => (
+                        <div
+                            key={index}
+                            className={message.user === 'User' ? 'user-message' : 'bot-message'}
+                        >
+                            {message.text}
+                        </div>
+                        ))}
+                        {isLoading && <LoadingAnimation />}
+                        <div ref={scrollToBottom}></div>
+                    </div>
                     </div>
                 </div>
 
                 <div className="row userInputClass">
                     <div className="col-lg-10">
-                        <input className="margin20" type="text" id="inputTextBox" name="inputTextBox"  placeholder="Type a new Question(eg:- Does my plan cover annual eye exams?)🙋‍♂️" />
+                        {/* <input className="margin20" type="text" value={userInput} id="inputTextBox" name="inputTextBox" onChange={e => setUserInput(e.target.value)} placeholder="Your question🙋‍♂️" /> */}
+
+                        <input className="margin20" type="text" value={userInput} id="inputTextBox" name="inputTextBox" onChange={e => setUserInput(e.target.value)} placeholder="Type a new Question(eg:- Does my plan cover annual eye exams?)🙋‍♂️" />
                     </div>
 
                     <div className="submitClass col-lg-2">
-                        <button type="button" class="btn btn-primary">Submit</button>
+                        <button type="button" onClick={backendCall} class="btn btn-primary">Submit</button>
                     </div>
                 </div>
 
