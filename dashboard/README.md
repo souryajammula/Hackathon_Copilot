1164074784cfd7e253ca2897613b5d1e448e434f
Sourya Jammula <souryaprateek2000@gmail.com>
2023-11-11 17:39:32 -0800

    Commiting into main

---
+++ dashboard/README.md
@@ -0,0 +1,1 @@
+Add pictures of UI here
---
+++ dashboard/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import Graph from './components/Graph'; // Import the Graph component
+import "./App.css";
+import ChatGPT from './components/chatGPT';
+
+function App() {
+  const [draggedSubheading, setDraggedSubheading] = useState(null);
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    const subheading = event.dataTransfer.getData('text/plain');
+    setDraggedSubheading(subheading);
+  };
+
+  const allowDrop = (event) => {
+    event.preventDefault();
+  };
+
+  const [visible, setVisible] = React.useState(false);
+  function click(e) { e.preventDefault(); setVisible(true); }
+
+  return (
+    <div className="app">
+      <Header />
+      <div className="main-content">
+        <Sidebar />
+        <div className="content"
+          onDrop={handleDrop}
+          onDragOver={allowDrop}
+        >
+          {draggedSubheading && <Graph subheading={draggedSubheading} />}
+        </div>
+      </div>
+      <footer className="footer">
+      {visible && (<ChatGPT />) } <img className="gpt" onClick={(event) => { click(event) }} src="C:\Users\soury\OneDrive\Desktop\dashboard\public\gpt1.png" alt="gpt"/>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
